2e63facd2d27d552b0a4eb49010f3e4531acfc8a
arkdru23 <arkadiusz.drukala@gmail.com>
2021-01-01 11:33:34 +0100

    add hide animationd when diamonds were matched

--- scripts/Game.esm.js
+++ scripts/Game.esm.js
@@ -18,6 +18,7 @@ const DIAMONDS_ARRAY_HEIGHT = DIAMONDS_ARRAY_WIDTH + 1;
 const LAST_ELEMENT_DIAMOND_ARRAY =
   DIAMONDS_ARRAY_WIDTH * DIAMONDS_ARRAY_HEIGHT - 1;
 const SWAPPING_SPEED = 8;
+const TRANSPARENCY_SPEED = 10;
 
 class Game extends Common {
   constructor() {
@@ -45,6 +46,7 @@ class Game extends Common {
     this.handleMouseClick();
     this.findMatches();
     this.moveDiamonds();
+    this.hideAnimation();
     this.countScores();
     this.revertSwap();
     this.clearMatched();
@@ -193,6 +195,19 @@ class Game extends Common {
       }
 
       if (dx || dy) {
+        this.gameState.setIsMoving(true);
+      }
+    });
+  }
+
+  hideAnimation() {
+    if (this.gameState.getIsMoving()) {
+      return;
+    }
+
+    this.gameState.getGameBoard().forEach((diamond) => {
+      if (diamond.match && diamond.alpha > TRANSPARENCY_SPEED) {
+        diamond.alpha -= 10;
         this.gameState.setIsMoving(true);
       }
     });
